a07cccc5f2ad708f2261d14719aa3e7f41c9d02f
kwa_laa <khoaddnguyen@gmail.com>
2023-12-04 13:53:31 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+def lengthOfLIS(self, nums: List[int]) -> int:
+    LIS = [1] * len(nums)  # length of the given input array
+
+    # in reverse order - starting from the last index back to zero
+    for i in range(len(nums) - 1, -1, -1):
+        for j in range(i + 1, len(nums)):
+            if nums[i] < nums[j]:
+                LIS[i] = max(LIS[i], 1 + LIS[j])
+    return max(LIS)
